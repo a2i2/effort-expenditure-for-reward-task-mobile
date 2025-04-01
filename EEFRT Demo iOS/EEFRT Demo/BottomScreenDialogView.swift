1c829df83d9d020dcb057d801800d5c7ea9ffe05
NicholasM365 <61439675+NicholasM365@users.noreply.github.com>
2025-04-01 11:45:15 +1100

    Add a basic bottom screen dialog to the iOS app

---
+++ EEFRT Demo iOS/EEFRT Demo/BottomScreenDialogView.swift
@@ -0,0 +1,94 @@
+import SwiftUI
+import WebKit
+
+struct BottomScreenDialogView: View {
+    private var titleText: String
+    private var subtitleText: String
+    private var actionButtonText: String
+    private var timeoutSeconds: TimeInterval
+    private var hostingWebview: WKWebView
+    private var dismissHandler: () -> Void
+    private var timeoutHandler: () -> Void
+    
+    init(titleText: String, subtitleText: String, actionButtonText: String, timeoutSeconds: TimeInterval, hostingWebview: WKWebView, dismissHandler: @escaping () -> Void, timeoutHandler: @escaping () -> Void) {
+        self.titleText = titleText
+        self.subtitleText = subtitleText
+        self.actionButtonText = actionButtonText
+        self.timeoutSeconds = timeoutSeconds
+        self.hostingWebview = hostingWebview
+        self.dismissHandler = dismissHandler
+        self.timeoutHandler = timeoutHandler
+    }
+    
+    var body: some View {
+        ZStack {
+            Color(hex: "#66000000")
+            
+            VStack {
+                Spacer()
+                
+                ZStack {
+                    UnevenRoundedRectangle(cornerRadii: RectangleCornerRadii(topLeading: 25, topTrailing: 25))
+                        .foregroundStyle(Color.white)
+                    
+                    VStack(spacing: 24) {
+                        ZStack {
+                            Text(titleText)
+                                .multilineTextAlignment(.center)
+                                .font(.system(size: 18, weight: .bold))
+                                .frame(maxWidth: .infinity)
+                            
+                            HStack {
+                                Spacer()
+                                
+                                TimeProgressView(
+                                    countdownSeconds: timeoutSeconds,
+                                    timeoutHandler: timeoutHandler
+                                )
+                                .padding(.trailing)
+                            }
+                        }
+                        .padding(.top)
+                        
+                        Text(subtitleText)
+                            .multilineTextAlignment(.center)
+                            .font(.system(size: 14, weight: .regular))
+                            .padding(.horizontal)
+                        
+                        Button {
+                            dismissHandler()
+                        } label: {
+                            ZStack {
+                                RoundedRectangle(cornerRadius: 25)
+                                    .stroke(Color.orange, lineWidth: 4)
+                                    .frame(height: 48)
+                                    .foregroundStyle(Color.white)
+
+                                Text(actionButtonText.uppercased())
+                                    .foregroundStyle(Color.orange)
+                                    .bold()
+                            }
+                        }
+                        .padding()
+                        .fixedSize(horizontal: false, vertical: false)
+                    }
+                    .padding(.vertical)
+                }
+                .fixedSize(horizontal: false, vertical: true)
+            }
+        }
+        .ignoresSafeArea(edges: .bottom)
+    }
+}
+
+#Preview {
+    BottomScreenDialogView(
+        titleText: "Are you still there?",
+        subtitleText: "Continue within the next 2 mins to keep collecting coins.",
+        actionButtonText: "Continue",
+        timeoutSeconds: 120.0,
+        hostingWebview: WKWebView(),
+        dismissHandler: {},
+        timeoutHandler: {}
+    )
+}
---
+++ EEFRT Demo iOS/EEFRT Demo/TimeProgressView.swift
@@ -0,0 +1,85 @@
+import SwiftUI
+import UIKit
+
+struct TimeProgressView: View {
+    private var countdownSeconds: Double
+    private var timeoutHandler: () -> Void
+    
+    @State private var timeLeft: Double
+    @State private var endAngle: Angle = .degrees(360)
+
+    private let clockBackgroundColor: Color = .init(hex: "#666666")
+    private let clockForegroundColor: Color = .init(hex: "#333333")
+    private let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+
+    init(countdownSeconds: Double, timeoutHandler: @escaping () -> Void) {
+        self.countdownSeconds = countdownSeconds
+        self.timeLeft = countdownSeconds
+        self.timeoutHandler = timeoutHandler
+    }
+
+    var body: some View {
+        ZStack {
+            HStack(spacing: 4) {
+                GeometryReader { geometry in
+                    ZStack {
+                        Path { path in
+                            let width: CGFloat = min(geometry.size.width, geometry.size.height)
+                            let height = width
+
+                            let center = CGPoint(x: width * 0.5, y: height * 0.5)
+
+                            path.move(to: center)
+
+                            path.addArc(center: center, radius: width * 0.5, startAngle: Angle(degrees: -90.0), endAngle: Angle(degrees: -90.0) + .degrees(360), clockwise: false)
+                        }
+                        .fill(clockBackgroundColor)
+
+                        Path { path in
+                            let width: CGFloat = min(geometry.size.width, geometry.size.height)
+                            let height = width
+
+                            let center = CGPoint(x: width * 0.5, y: height * 0.5)
+
+                            path.move(to: center)
+
+                            path.addArc(center: center, radius: width * 0.5, startAngle: Angle(degrees: -90.0), endAngle: Angle(degrees: -90.0) + endAngle, clockwise: false)
+                        }
+                        .fill(clockForegroundColor)
+                    }
+                }
+                .frame(width: 20, height: 20, alignment: .center)
+                .aspectRatio(1, contentMode: .fit)
+                .onReceive(timer) { _ in
+                    if timeLeft == 0 {
+                        timer.upstream.connect().cancel()
+                    } else {
+                        timeLeft -= 1
+                        endAngle = Angle(degrees: Double((timeLeft / countdownSeconds) * 360))
+                    }
+                }
+
+                Text("\(timeFormatter(timeRemaining: timeLeft))")
+                    .font(Font.custom("Roboto-Bold", size: 16))
+            }
+        }
+        .padding(.horizontal, 8)
+        .padding(.vertical, 4)
+        .background(Color.gray.opacity(0.2))
+        .clipShape(RoundedRectangle(cornerRadius: 12))
+    }
+
+    func timeFormatter(timeRemaining: Double) -> String {
+        let roundedTime = Int(timeRemaining)
+        let minutes = roundedTime / 60
+        let seconds = roundedTime % 60
+
+        return seconds < 10 ? "\(minutes):0\(seconds)" : "\(minutes):\(seconds)"
+    }
+}
+
+struct TimeProgressView_Previews: PreviewProvider {
+    static var previews: some View {
+        TimeProgressView(countdownSeconds: 120, timeoutHandler: {})
+    }
+}
