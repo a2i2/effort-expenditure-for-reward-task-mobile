26339294eb926ab438dafb8c43eb10d1d07031e1
Joost Funke Kupper <joostfunkekupper@gmail.com>
2025-04-17 12:00:46 +1000

    Clean up coins.js file

--- public/src/elements/coins.js
+++ public/src/elements/coins.js
@@ -11,30 +11,28 @@ export default class Coins {
             frameRate: 10,   // display 10 frames per second
             repeat: -1       // loop animation
         });
-        
+
         // create static physics group (allows multiple coins)
-        this.sprite = scene.physics.add.staticGroup({    
-        key: 'coin',                               
-        repeat: nCoins-1,    // creates repeat+1 children
-        setXY: { x: x,   
-                 y: y, 
-                 stepX: 30   // scatters children along X with this step size
-               }                        
-        });
-        
-        var rewardCoins = this.sprite; 
-        rewardCoins.children.iterate(function (child) {
-            child.setScale(1.5);      // increase in size a bit
-            child.play('coinspin');
+        this.sprite = scene.physics.add.staticGroup({
+            key: 'coin',
+            repeat: nCoins-1,    // creates repeat+1 children
+            setXY: {
+                x: x,
+                y: y,
+                stepX: 30   // scatters children along X with this step size
+            }
         });
 
+        var rewardCoins = this.sprite; 
+        rewardCoins.children.iterate((child) => {
+            child.setScale(1.5);
+            child.play('coinspin');
+        });
     }
 
     update() { }
-    
-    destroy() {    
-        var allCoins = this.sprite.getChildren();
-        allCoins.destroy();
-    }
 
+    destroy() {
+        this.sprite.clear(true, true);
+    }
 }
