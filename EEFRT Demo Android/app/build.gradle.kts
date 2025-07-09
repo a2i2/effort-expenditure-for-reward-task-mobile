dca1813bb11e3386bef4a206bc705312d37b4728
Joost Funke Kupper <joostfunkekupper@gmail.com>
2025-07-09 19:49:45 +1000

    Increment build number

--- EEFRT Demo Android/app/build.gradle.kts
+++ EEFRT Demo Android/app/build.gradle.kts
@@ -69,7 +69,7 @@ android {
         applicationId = "au.org.blackdoginstitute.effrtdemo"
         minSdk = 28
         targetSdk = 35
-        versionCode = 3
+        versionCode = 4
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
